2804fb9286b9625af9d155de02c29ed3cd9c87f5
janani.sampathkumar <janani.sampathkumar@mirriad.com>
2019-05-18 11:45:27 +0100

    Slice 4 : Adding test for Price Validation

---
+++ src/test/java/TillDisplayCalculatorTest.java
@@ -0,0 +1,26 @@
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+public class TillDisplayCalculatorTest {
+
+    TillDisplayCalculator tillDisplayCalculator = new TillDisplayCalculator();
+
+    @Test
+    public void shouldNotAcceptPriceFor4DecimalPlaces() {
+        BigDecimal price = new BigDecimal("23.3434");
+        assert !tillDisplayCalculator.isPriceValid(price);
+    }
+
+    @Test
+    public void shouldAcceptPriceFor2DecimalPlaces() {
+        BigDecimal price = new BigDecimal("23.33");
+        assert tillDisplayCalculator.isPriceValid(price);
+    }
+
+    @Test
+    public void shouldNotAcceptPriceFor1DecimalPlaces() {
+        BigDecimal price = new BigDecimal("23.3");
+        assert !tillDisplayCalculator.isPriceValid(price);
+    }
+}
