a3ab070eae15150b8d63e268b9b792aaec197eab
janani.sampathkumar <janani.sampathkumar@mirriad.com>
2019-05-18 15:52:23 +0100

    Slice 6 : Refactoring

--- src/main/java/com/till/display/TillDisplayCalculator.java
+++ src/main/java/com/till/display/TillDisplayCalculator.java
@@ -1,14 +1,12 @@
 package com.till.display;
 
-import java.util.Map;
-
 public class TillDisplayCalculator {
 
     public static void main(String[] args) {
 
         System.out.println("Hello ! Welcome to the Till...");
         UserInput userInput = new UserInput();
-        Map inputMap = userInput.getNValidateUserInput();
+        userInput.getUserInput();
 
     }
 
--- src/main/java/com/till/display/UserInput.java
+++ src/main/java/com/till/display/UserInput.java
@@ -7,22 +7,33 @@ import java.util.Scanner;
 
 public class UserInput {
 
-    public Map getNValidateUserInput() {
-        Map<String, BigDecimal> itemPriceMap = new HashMap<>();
+    private int noOfItems;
+    private Map<String, BigDecimal> itemPriceMap;
+    private String stateCode;
 
+    public void getUserInput() {
         Scanner scanner = new Scanner(System.in);
+        setNValidateNoOfItems(scanner);
+        setNValidateItemPrice(scanner);
+        setNValidateStateCode(scanner);
+    }
 
+    public void setNValidateNoOfItems(Scanner scanner) {
         System.out.println("Please enter the number of items (in number) : ");
         System.out.println("----------------------------------------------------------------");
-        int noOfItems;
+
         while (!scanner.hasNextInt()) {
             String itemNumberInput = scanner.next();
             System.out.println("Sorry, invalid input ! Please enter a valid input in number.");
         }
         noOfItems = scanner.nextInt();
+    }
 
+    public void setNValidateItemPrice(Scanner scanner) {
         System.out.println("----------------------------------------------------------------");
         System.out.println("Please enter the item name and price (in $) for all the items : ");
+
+        itemPriceMap = new HashMap<>();
         for (int i = 0; i < noOfItems; i++) {
 
             System.out.println("Item Name : ");
@@ -37,20 +48,30 @@ public class UserInput {
                 }
                 price = scanner.nextBigDecimal();
             } while (!isPriceDecimalValid(price));
-
             itemPriceMap.put(itemName, price);
         }
+    }
 
+    public void setNValidateStateCode(Scanner scanner){
         System.out.println("----------------------------------------------------------------");
         System.out.println("Please enter the state code (UT, NV, TX, AL, CA) : ");
-        String stateCode = null;
         do {
             stateCode = scanner.next();
         } while (!isStateCodeValid(stateCode));
 
         System.out.println("----------------------------------------------------------------");
+    }
 
+    public int getNoOfItems() {
+        return noOfItems;
+    }
+
+    public Map<String, BigDecimal> getItemPriceMap() {
         return itemPriceMap;
+    }
+
+    public String getStateCode() {
+        return stateCode;
     }
 
     public boolean isPriceDecimalValid(BigDecimal price) {
