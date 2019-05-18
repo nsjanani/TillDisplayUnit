a293ac854d45bf33a9e4b3c613bd3c7a7d7870e0
janani.sampathkumar <janani.sampathkumar@mirriad.com>
2019-05-18 11:44:30 +0100

    Slice 3 : Validate Price for exactly upto 2 decimal places

--- src/main/java/TillDisplayCalculator.java
+++ src/main/java/TillDisplayCalculator.java
@@ -1,6 +1,4 @@
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.util.Currency;
 import java.util.Scanner;
 
 public class TillDisplayCalculator {
@@ -13,14 +11,27 @@ public class TillDisplayCalculator {
 
     public void getInput() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Please enter the number of items : ");
+        System.out.println("Please enter the number of items (in number) : ");
         int noOfItems = scanner.nextInt();
         System.out.println("Please enter the item name and price (in $) for all the items : ");
         for(int i=0; i<noOfItems; i++) {
             System.out.println("Item Name : ");
             String itemName = scanner.next();
-            System.out.println("Price : ");
+            System.out.println("Price (in $) : ");
             BigDecimal price = scanner.nextBigDecimal();
+            if(isPriceValid(price))
+                continue;
+            else
+                break;
         }
+    }
+
+    public boolean isPriceValid(BigDecimal price) {
+       if(price.scale() == 2)
+           return true;
+       else {
+           System.out.println("Sorry ! Exiting... Price input should be exactly 2 decimal places");
+           return false;
+       }
     }
 }
