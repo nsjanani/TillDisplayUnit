9fd96d3516d83f497af65cec206efeb18fd96adf
janani.sampathkumar <janani.sampathkumar@mirriad.com>
2019-05-25 11:07:07 +0100

    Slice 9 : Discount calculation

--- src/main/java/com/till/display/PriceCalculator.java
+++ src/main/java/com/till/display/PriceCalculator.java
@@ -2,6 +2,8 @@ package com.till.display;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Map;
 
 public class PriceCalculator {
@@ -11,13 +13,17 @@ public class PriceCalculator {
         for(Map.Entry<String, BigDecimal> itemEntry : itemMap.entrySet()) {
             totalPrice = totalPrice.add(itemEntry.getValue());
         }
-        System.out.println("Your Total Payment (in $)                    ");
-        System.out.println("---------------------------------------------");
-        System.out.println("Total Price (in $ without tax): " + totalPrice);
+        System.out.println("Your Total Payment                           ");
+        System.out.println("----------------------------------------------------------------");
+        System.out.println("Total Items Price             " + currencyFormat(totalPrice));
+        BigDecimal discountPrice = getDiscountPrice(totalPrice);
+        System.out.println("Discount                   -  " + currencyFormat(discountPrice));
+        totalPrice = totalPrice.subtract(discountPrice);
+        System.out.println("Price after discount          " + currencyFormat(totalPrice));
         BigDecimal taxPrice = getStateTaxPrice(stateCode, totalPrice);
-        System.out.println("Tax Price (in $):               " + taxPrice);
+        System.out.println("Tax                        +  " + currencyFormat(taxPrice));
         totalPrice = totalPrice.add(taxPrice);
-        System.out.println("Total Price to pay (in $)       " + totalPrice);
+        System.out.println("Total Amount to pay           " + currencyFormat(totalPrice));
     }
 
     public BigDecimal getStateTaxPrice(String stateCode, BigDecimal totalPrice) {
@@ -25,5 +31,26 @@ public class PriceCalculator {
         BigDecimal stateTaxPrice = totalPrice.multiply(stateTaxPercent);
         stateTaxPrice = stateTaxPrice.setScale(2, RoundingMode.FLOOR);
         return stateTaxPrice;
+    }
+
+    public BigDecimal getDiscountPrice(BigDecimal totalPrice) {
+        double price = totalPrice.doubleValue();
+        double discount_rate = 0;
+        if (price >= 150000)
+            discount_rate = 0.15;
+        else if (price >= 10000)
+            discount_rate = 0.1;
+        else if (price >= 7000)
+            discount_rate = 0.07;
+        else if (price >= 5000)
+            discount_rate = 0.05;
+        else if (price >= 1000)
+            discount_rate = 0.03;
+
+        return totalPrice.multiply(new BigDecimal(discount_rate)).setScale(2, RoundingMode.FLOOR);
+    }
+
+    public static String currencyFormat(BigDecimal n) {
+        return NumberFormat.getCurrencyInstance(Locale.US).format(n);
     }
 }
